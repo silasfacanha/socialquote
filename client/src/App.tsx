c1b4485c6959e8709bed8182b03a90d6fbb54edb
silasfacanha <silasfacanha@hotmail.com>
2023-06-19 17:21:51 +0100

    feat: add a footer to all pages. It is not responsive yet. closes: #5

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 
 import { Container, CssBaseline } from "@mui/material";
 import Navbar from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <Container maxWidth={"xl"}>
       <Navbar />
+      <Footer/>
     </Container>
   
   );
