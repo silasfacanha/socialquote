2b70abe91a9fda696b5a36b3d7a73f745416166d
silasfacanha <silasfacanha@hotmail.com>
2023-06-19 21:08:22 +0100

    feat: add the home page. it is not responsive yet and the fetching functionality must be implemented closes #7

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { Container, TextField, Typography } from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Container sx={{margin: "10%"} }>
+ <TextField
+      sx={{
+        display: 'flex'
+      }}
+      multiline
+      rows={4}
+      variant="outlined"
+      label="Write a post"
+    />
+    <Container sx={{height: '80vh' }}> 
+
+<Container sx={{display:'flex', justifyContent:"center"}}> <Typography > Fetched Posts</Typography></Container>
+    </Container>
+    </Container>
+  )
+}
+
+export default Home
