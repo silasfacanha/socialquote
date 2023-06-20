9aa6a75295cf1d545534b1e90f035d92032defec
silas <silasfacanha@hotmail.com>
2023-06-20 06:19:33 +0100

    feat: add the myOracula page. It is not responsive yet. It still needed to add the fetching functionality.                                                    Closes #9

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,15 +1,26 @@
-import { Box, Container, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Container, Typography } from "@mui/material";
+import React from "react";
 
 export const Footer = () => {
   return (
-    <Box sx={{height:'100px', backgroundColor:'black', color:'white', display: 'flex', alignItems:'center'}}>
-        <Container sx={{ display: 'flex', justifyContent:'space-between'}}> <Typography> Help </Typography>
+    <Box
+      sx={{
+        height: "100px",
+        backgroundColor: "black",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <Container sx={{ display: "flex", justifyContent: "space-between" }}>
+        {" "}
+        <Typography> Help </Typography>
         <Typography> Contact </Typography>
         <Typography> About </Typography>
         <Typography> Terms </Typography>
         <Typography> Privacy </Typography>
-        </Container>
+      </Container>
     </Box>
-  )
-}
+  );
+};
---
+++ client/src/pages/MyOracula.tsx
@@ -0,0 +1,26 @@
+import { Container, Paper, Typography } from "@mui/material";
+import React from "react";
+
+const MyOracula = () => {
+  return (
+    <Container
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <Paper sx={{ display: "flex", justifyContent: "center" }}>
+        <Typography> Here are your Oracula!</Typography>
+        <Typography> User Oracula</Typography>
+      </Paper>
+      <Paper sx={{ display: "flex", justifyContent: "center" }}>
+        <Typography> Here are your Quotes!</Typography>
+        <Typography> User Quotes</Typography>
+      </Paper>
+    </Container>
+  );
+};
+
+export default MyOracula;
